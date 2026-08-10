44c323b24aa497db56366542b522ab45fb202d6a
Shreyansh Yadav <shrey0031@outlook.com>
2026-08-10 18:10:19 +0530

    Update import statement for HumanMessage and AIMessage

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,7 @@ if os.name == 'posix':
 from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
 from langchain_anthropic import ChatAnthropic
-from langchain.schema import HumanMessage, AIMessage
-
+from langchain_core.messages import HumanMessage, AIMessage
 from rag_utils import (
     load_doc_to_db, 
     load_url_to_db, 
